f4e40364f9bc2f887122f58d8ada464dbdaa457b
Aadarsh-00 <aadarsh.karn@westcliff.edu>
2025-11-12 18:39:08 +0545

    Question 1 code | Added

---
+++ Week-2/typeCasting1.py
@@ -0,0 +1,7 @@
+num = float(input("Kunai float(decimal) number halnuhosh: "))
+
+print("Tapaile deko float number: ", num)
+int_num = int(num)
+print("Tapaile deko float number ko integer rupantran: ", int_num)
+str_num = str(int_num)
+print("Tapaile deko float number ko string rupantran: ", str_num)
