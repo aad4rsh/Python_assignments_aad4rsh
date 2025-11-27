981b21a988d25cd64febc53b3c81b7294ad81d3c
aad4rsh <160861620+aad4rsh@users.noreply.github.com>
2025-11-27 22:36:56 +0545

    Delete Assignment-1 directory

--- Assignment-1/typeCasting1.py
+++
@@ -1,7 +0,0 @@
-num = float(input("Kunai float(decimal) number halnuhosh: "))
-
-print("Tapaile deko float number: ", num)
-int_num = int(num)
-print("Tapaile deko float number ko integer rupantran: ", int_num)
-str_num = str(int_num)
-print("Tapaile deko float number ko string rupantran: ", str_num)
--- Assignment-1/wordSlicing3.py
+++
@@ -1,7 +0,0 @@
-
-text = input("Kehi sabda lekhnuhos: ")
-
-index  = input("Kun index bata slice garne ho? ")
-
-sliced = text[int(index):]
-print("Slice garepachi: "+sliced)
